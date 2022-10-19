14af4cc4a4766e620923890da2b44dc7b5863572
Royalty007-dev <topbusari2008@gmail.com>
2022-10-18 18:44:36 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
+	int ten;
+	char la;
 
-		char num, letter;
-
-			for (num = 0; num <= 9; num++)
-			{
-				for (letter = 'a'; letter <= 'z'; letter++)					{
-															_putchar(letter);
-				}
-				  _putchar('\n');								}
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
 }
