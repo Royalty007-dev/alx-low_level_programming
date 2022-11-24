c49b3664a7f2a5b56367272e1e0b0c75ee4ffa91
Royalty007-dev <topbusari2008@gmail.com>
2022-11-24 13:12:12 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,14 @@ size_t print_list(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-	 while (h != NULL)
-	 {
-		 if (h->str != NULL)
-			 printf("[%u] %s\n", h->len, h->str);
-		 else
-			 printf("[0] (nil)\n");
-		 h = h->next;
-		 i++;
-	 }
-	 return (i);
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
